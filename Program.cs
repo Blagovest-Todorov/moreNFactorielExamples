53fae06fee739360d1424b3eefff42f62f8aea08
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-13 09:17:34 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09.WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            uint n = Convert.ToUInt32(Console.ReadLine());
+            Console.Write("n! = ");
+
+            uint result = 1;
+
+            while (true)
+            {
+                Console.Write(n);
+                if (n == 1)
+                {
+                    break;
+                }
+                Console.Write("*");
+                result *= n;
+                n--;
+
+            }
+            Console.WriteLine("= {0}", result);
+
+
+
+        }
+    }
+}
